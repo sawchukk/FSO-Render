f8d0dd3a5285b625a7547bc41890473ed878b718
sawchukk <sawchukk@gmail.com>
2024-11-04 14:17:57 -0800

    update port w/ env

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ const generateId = () => {
 
 //app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, ()=>{
     console.log(`server running on port ${PORT}`)
 })
